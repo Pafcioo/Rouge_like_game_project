4c1d7611dfc0b0f77b5edcf3c46eb27dcca901a9
Pafcioo <pzawalniak@gmail.com>
2025-04-27 19:17:47 +0200

    created sfml window

--- Game/GameManager.h
+++ Game/GameManager.h
@@ -1,14 +1,17 @@
 #pragma once
 #include <SFML/Graphics.hpp>
+#include "EntityManager.h"
 
 class GameManager
 {
 private:
     sf::RenderWindow gameWindow;
     sf::Clock gameClock;
+    EntityManager entityManager;
+
 public:
-    GameManager();
-    ~GameManager();
+    GameManager(){};
+    ~GameManager(){};
 
     void Play();
 };
